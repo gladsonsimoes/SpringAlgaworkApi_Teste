c935fbd1673bffa09dbc88c03392c3cf0742c6c8
Gladson Simões <gladsonssimoes@outlook.com.br>
2022-09-06 21:43:25 -0300

    Add files via upload

--- 3.3 Migracao de dados com Flyway - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ 3.3 Migracao de dados com Flyway - Spring Boot 2.7.0/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,10 +3,12 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 //implementar os metodos
@@ -17,8 +19,21 @@ public class CategoriaResource {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    @GetMapping
-    public List<Categoria> listar(){
+    @GetMapping //endpoint Get
+    public List<Categoria> listar(){ //retorna a lista de categoria
         return categoriaRepository.findAll();
     }
+    @PostMapping //endpoint post
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria , HttpServletResponse response){ //retorna apenas a categoria
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") //para criar a url para o postman
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri(); //getcodigo para ver o somente o codigo
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+    @GetMapping("/{codigo}") //endpoint get pelo codigo , {codigo} é para especificar o codigo depois /categorias
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo){ //@PathVariable para buscar categoria especifica
+        return categoriaRepository.findById(codigo).orElse(null);
+    }
+
 }
